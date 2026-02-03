522d5323e9d1c538221556e65f2c495078b0ea82
Amber Yang <yamber0302@qq.com>
2026-02-03 21:36:58 +0800

    style: 修改布局样式

--- frontend/src/app/weekly/page.tsx
+++ frontend/src/app/weekly/page.tsx
@@ -2,8 +2,6 @@ import WeeklyReportModule from "@/modules/weekly";
 
 export default function WeeklyPage() {
   return (
-    <div className="h-[calc(100vh-4rem)] w-full p-4 bg-slate-50">
       <WeeklyReportModule />
-    </div>
   );
 }
